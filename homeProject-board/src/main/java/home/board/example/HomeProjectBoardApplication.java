96b0e07d9e9d3402f1133104e47c5c1ed9e20114
DEEPBLUE104 <dlara7173@gmail.com>
2025-05-03 21:42:06 +0900

    homeProject-board

---
+++ homeProject-board/src/main/java/home/board/example/HomeProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package home.board.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeProjectBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeProjectBoardApplication.class, args);
+	}
+
+}
---
+++ homeProject-board/src/test/java/home/board/example/HomeProjectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package home.board.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeProjectBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
