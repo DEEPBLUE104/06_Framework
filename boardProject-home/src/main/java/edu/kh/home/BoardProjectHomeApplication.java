cf7942c6541cdf32f0088ff0c4911c916a49fc7a
DEEPBLUE104 <dlara7173@gmail.com>
2025-05-05 12:10:53 +0900

    homeboard

---
+++ boardProject-home/src/main/java/edu/kh/home/BoardProjectHomeApplication.java
@@ -0,0 +1,13 @@
+package edu.kh.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardProjectHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardProjectHomeApplication.class, args);
+	}
+
+}
--- homeProject-board/src/main/java/home/board/example/HomeProjectBoardApplication.java
+++ homeProject-board/src/main/java/home/board/example/HomeProjectBoardApplication.java
@@ -2,8 +2,9 @@ package home.board.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {SecurityAutoConfiguration.class})
 public class HomeProjectBoardApplication {
 
 	public static void main(String[] args) {
