5ceaef2bf8adca42872971e2941fd0464649423d
DEEPBLUE104 <dlara7173@gmail.com>
2025-05-16 10:13:31 +0900

    웹소켓

---
+++ boardProject-boot/src/main/java/edu/kh/project/websocket/handler/TestWebSocketHandler.java
@@ -0,0 +1,17 @@
+package edu.kh.project.websocket.handler;
+
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+/** 000WebSocketHandler 클래스
+ * 
+ * 웹소켓 동작 시 수행할 구문을 작성하는 클래스
+ * 
+ */
+@Slf4j
+@Component // Bean 등록
+public class TestWebSocketHandler {
+
+}
+
